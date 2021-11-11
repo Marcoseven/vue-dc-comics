d5c38fcf6d099882385d79d144d9b84319f89868
Marcoseven <marco.caliciotti3@gmail.com>
2021-11-11 13:44:30 +0100

    Creata struttura di base
    Creata struttura usando Vue CLI, ed inseriti nella cartella components tre file vue mentre nella cartella assets inserite le cartelle img e scss.

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+	<div id="app">
+		<SiteHeader />
+		<!-- /#SiteHeader -->
+
+		<SiteMain />
+		<!-- /#SiteMain -->
+
+		<SiteFooter />
+		<!-- /#SiteFooter -->
+	</div>
+</template>
+
+<script>
+import SiteHeader from "./components/SiteHeader.vue";
+import SiteMain from "./components/SiteMain.vue";
+import SiteFooter from "./components/SiteFooter.vue";
+
+export default {
+	name: "App",
+	components: {
+		SiteHeader,
+		SiteMain,
+		SiteFooter,
+	},
+};
+</script>
+
+<style lang="scss">
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
