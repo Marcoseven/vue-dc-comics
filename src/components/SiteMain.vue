1f8627d5d83c17de5ad86af4936e7cf9a475b958
Marcoseven <marco.caliciotti3@gmail.com>
2021-11-12 15:17:15 +0100

    Aggiunte modifiche file vue (SiteMain)
    Inserite delle modifiche alla parte script, con l'array di oggetti e style.

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,81 +1,191 @@
 <template>
 	<main id="SiteMain">
-		<div class="container">
-			<!-- .rowUp -->
-			<div class="rowUp row">
-				<div id="here">--> Content goes here</div>
-			</div>
-			<!-- /.rowUp -->
-
-			<!-- .rowCards -->
-			<div class="rowCards">
-				<div class="col-2">
-					<a href="#">
-						<img src="../assets/img/buy-comics-digital-comics.png" alt="" />
-						<h6>DIGITAL COMICS</h6>
-					</a>
-				</div>
-				<div class="col-2">
-					<a href="#">
-						<img src="../assets/img/buy-comics-merchandise.png" alt="" />
-						<h6>DC MERCHANDISE</h6>
-					</a>
-				</div>
-				<div class="col-2">
-					<a href="#">
-						<img src="../assets/img/buy-comics-subscriptions.png" alt="" />
-						<h6>SUBSCRIPTION</h6>
-					</a>
-				</div>
-				<div class="col-2">
-					<a href="#">
-						<img src="../assets/img/buy-comics-shop-locator.png" alt="" />
-						<h6>COMIC SHOP LOCATOR</h6>
-					</a>
-				</div>
-				<div class="col-2">
-					<a href="#">
-						<img src="../assets/img/buy-dc-power-visa.svg" alt="" />
-						<h6>DC POWER VISA</h6>
-					</a>
+		<div id="jumbo"></div>
+		<!-- .rowUp -->
+		<div class="rowUp row">
+			<div class="container">
+				<div class="row">
+					<Product
+						v-for="card in cards"
+						:key="card.series"
+						:image="card.thumb"
+						:titolo="card.series"
+						:cardType="card.type"
+						class="col-2"
+					/>
 				</div>
 			</div>
-			<!-- /.rowCards -->
 		</div>
+		<!-- /.rowUp -->
+
+		<!-- .rowCards -->
+		<div class="rowCards">
+			<div class="col-2">
+				<a href="#">
+					<img src="../assets/img/buy-comics-digital-comics.png" alt="" />
+					<h6>DIGITAL COMICS</h6>
+				</a>
+			</div>
+			<div class="col-2">
+				<a href="#">
+					<img src="../assets/img/buy-comics-merchandise.png" alt="" />
+					<h6>DC MERCHANDISE</h6>
+				</a>
+			</div>
+			<div class="col-2">
+				<a href="#">
+					<img src="../assets/img/buy-comics-subscriptions.png" alt="" />
+					<h6>SUBSCRIPTION</h6>
+				</a>
+			</div>
+			<div class="col-2">
+				<a href="#">
+					<img src="../assets/img/buy-comics-shop-locator.png" alt="" />
+					<h6>COMIC SHOP LOCATOR</h6>
+				</a>
+			</div>
+			<div class="col-2">
+				<a href="#">
+					<img src="../assets/img/buy-dc-power-visa.svg" alt="" />
+					<h6>DC POWER VISA</h6>
+				</a>
+			</div>
+		</div>
+		<!-- /.rowCards -->
 	</main>
 </template>
 
-<script></script>
+<script>
+import Product from "./Cards";
+export default {
+	components: {
+		Product,
+	},
+	data() {
+		return {
+			cards: [
+				{
+					thumb:
+						"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
+					price: "$19.99",
+					series: "Action Comics",
+					type: "comic book",
+				},
+				{
+					thumb:
+						"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
+					price: "$3.99",
+					series: "American Vampire 1976",
+					type: "comic book",
+				},
+				{
+					thumb:
+						"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb",
+					price: "$16.99",
+					series: "Aquaman",
+					type: "graphic novel",
+				},
+				{
+					thumb:
+						"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76",
+					price: "$2.99",
+					series: "Batgirl",
+					type: "comic book",
+				},
+				{
+					thumb:
+						"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+					price: "$3.99",
+					series: "Batman",
+					type: "comic book",
+				},
+				{
+					thumb:
+						"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+					price: "$2.99",
+					series: "Batman Beyond",
+					type: "comic book",
+				},
+				{
+					thumb:
+						"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8",
+					price: "$3.99",
+					series: "Batman/Superman",
+					type: "comic book",
+				},
+				{
+					thumb:
+						"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk",
+					price: "$4.99",
+					series: "Batman/Superman Annual",
+					type: "comic book",
+				},
+				{
+					thumb:
+						"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8",
+					price: "$5.99",
+					series: "Batman: The Joker War Zone",
+					type: "comic book",
+				},
+				{
+					thumb:
+						"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY",
+					price: "$6.99",
+					series: "Batman: Three Jokers",
+					type: "comic book",
+				},
+				{
+					thumb:
+						"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX",
+					price: "$4.99",
+					series: "Batman: White Knight Presents: Harley Quinn",
+					type: "comic book",
+				},
+				{
+					thumb:
+						"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq",
+					price: "$16.99",
+					series: "Catwoman",
+					type: "graphic novel",
+				},
+			],
+		};
+	},
+};
+</script>
 
 <style lang="scss">
 @import "../assets/scss/common.scss";
 @import "../assets/scss/variables.scss";
 
 #SiteMain {
-	height: 50vh;
+	height: auto;
 	position: relative;
 	z-index: 10;
-	background-image: linear-gradient(
-		to bottom,
-		#000 0px,
-		#000 40%,
-		$brand_color 40%,
-		$brand_color 100%
-	);
+	#jumbo {
+		height: 330px;
+		background-image: url("../assets/img/jumbotron.jpg");
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
 
 	.rowUp {
-		height: 45%;
+		height: auto;
 		align-items: center;
 		justify-content: start !important;
-		#here {
-			color: #fff;
+		flex-wrap: wrap;
+		background-color: #000;
+
+		.row {
+			flex-wrap: wrap;
 		}
 	}
 	.rowCards {
-		height: 55%;
+		height: auto;
 		display: flex;
 		align-items: center;
 		justify-content: space-around;
+		background-color: $brand_color;
 
 		.cards-2 {
 			align-items: center;
