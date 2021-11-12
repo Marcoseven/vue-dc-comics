56213cc76a7d15f2788ae554722076b321dc85ab
Marcoseven <marco.caliciotti3@gmail.com>
2021-11-12 15:14:07 +0100

    Creato file vue (componente Cards)
    Inserita la struttura di base, più il props ed altro.

---
+++ src/components/Cards.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<div class="card">
+			<img :src="image" alt="" />
+			<h3>{{ titolo }}</h3>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		image: String,
+		titolo: String,
+	},
+};
+</script>
+
+<style lang="scss">
+.card {
+	color: #fff;
+	text-align: center;
+	h3 {
+		padding: 0.5rem;
+	}
+}
+</style>
